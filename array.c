fbb0b9a79f9e006ab084b1ce2c4c839c68831b88
SeanMarket <sean@marketos.com>
2026-03-09 17:33:24 -0600

    first commit

---
+++ array.c
@@ -0,0 +1,7 @@
+#include "array.h"
+
+
+int array_init(array *s){
+    s->front = -1;
+    s->back = -1;
+}
---
+++ array.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ARR_SIZE 8
+typedef struct{
+    char arr[ARR_SIZE];
+    int front;
+    int back; //front and back indeces for circular queue implementation
+} array;
+
+
+int array_init(array *s);                 // initialise the array
+int array_put(array *s, char *hostname);  // place element into the array, block when full
+int array_get(array *s, char **hostname); // remove element from the array, block when empty
+void array_free(array *s);                // free the array's resources
