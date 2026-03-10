10ef2c2e24add5a538dd1b7ae95f7abb9a2af991
SeanMarket <sean@marketos.com>
2026-03-09 18:11:18 -0600

    Finished basic implementation. Needs thread-safety features

--- array.c
+++ array.c
@@ -1,7 +1,64 @@
 #include "array.h"
 
+bool array_isFull(array *s){
+    if(s->back + 1 % ARR_SIZE == s->front){
+        return true;
+    }
+    return false;
+}
+
+bool array_isEmpty(array *s){
+    if((s->front == -1) && (s->back == -1)){
+        return true;
+    }
+    return false;
+}
+
 
 int array_init(array *s){
     s->front = -1;
     s->back = -1;
+    for (int i = 0; i < ARR_SIZE; i++){
+        s->arr[i] = NULL;
+    }
+    return 0;
+}
+
+int array_put(array *s, char *hostname){
+    if(array_isFull(s)){
+        printf("Cannot put. Array is full\n");
+        return -1;
+    }
+    else if(array_isEmpty(s)){
+        s->front++;
+    }
+
+    s->back = (s->back + 1) % ARR_SIZE;
+    s->arr[s->back] = hostname;
+}
+
+int array_get(array *s, char **hostname){
+    if(array_isEmpty(s)){
+        printf("Cannot get. Array is empty\n");
+        return -1;
+    }
+
+    hostname = s->arr[s->front];
+
+    if(s->front == s->back){
+        s->front = s->back = -1;
+    }
+    else{
+        s->front = (s->front + 1) % ARR_SIZE;
+    }
+
+    return 0;
+}
+
+void array_free(array *s){
+    for(int i = 0; i < ARR_SIZE; i++){
+        s->arr[i] = NULL;
+    }
+    s->front == -1;
+    s->back == -1;
 }
--- array.h
+++ array.h
@@ -2,10 +2,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define ARR_SIZE 8
 typedef struct{
-    char arr[ARR_SIZE];
+    char* arr[ARR_SIZE];
     int front;
     int back; //front and back indeces for circular queue implementation
 } array;
@@ -15,3 +16,5 @@ int array_init(array *s);                 // initialise the array
 int array_put(array *s, char *hostname);  // place element into the array, block when full
 int array_get(array *s, char **hostname); // remove element from the array, block when empty
 void array_free(array *s);                // free the array's resources
+bool array_isFull(array *s);
+bool array_isEmpty(array *s);
